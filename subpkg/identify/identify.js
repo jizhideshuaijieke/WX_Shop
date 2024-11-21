6d26d97004baeb5b38fb93b74cce2906dbcb2974
jizhideshuaijieke <2550467835@qq.com>
2024-11-21 21:20:24 +0800

    细微调整

--- subpkg/identify/identify.js
+++ subpkg/identify/identify.js
@@ -27,10 +27,10 @@ Page({
     }
     return btoa(binary); // 使用 btoa 将二进制字符串转换为 Base64
   },
-  // 绘制定位框的函数，接收后端返回的框信息
+
   drawMarks(frames) {
     const query = wx.createSelectorQuery();
-    // 获取 Canvas 节点
+
     query.select('#actionMarks')
       .fields({
         node: true,
@@ -38,28 +38,25 @@ Page({
       }) // 获取 Canvas 节点和尺寸
       .exec((res) => {
         const canvas = res[0].node; // 获取 Canvas 节点
-        const ctx = canvas.getContext('2d'); // 获取 Canvas 的 2d 渲染上下文
-        canvas.width = res[0].width; // 设置 Canvas 宽度
-        canvas.height = res[0].height; // 设置 Canvas 高度
+        const ctx = canvas.getContext('2d');
+        canvas.width = res[0].width; // 设置 Canvas 绘制区域宽度
+        canvas.height = res[0].height; // 设置 Canvas 绘制区域高度
 
         // 清空画布
         ctx.clearRect(0, 0, canvas.width, canvas.height);
 
         // 遍历后端返回的框信息，绘制每个定位框
         frames.forEach(frame => {
-          // 提取框的信息，如种类、坐标、大小等
           const { type, x, y, width, height} = frame;
-
-          // 根据框的类型设置不同的颜色或样式
           if (type === 'type1') {
-            ctx.setStrokeStyle('#ff0000'); // 红色框
+            ctx.setStrokeStyle('#ff0000');
           } else if (type === 'type2') {
-            ctx.setStrokeStyle('#00ff00'); // 绿色框
+            ctx.setStrokeStyle('#00ff00');
           } else {
-            ctx.setStrokeStyle('#0000ff'); // 默认蓝色框
+            ctx.setStrokeStyle('#0000ff');
           }
 
-          ctx.setLineWidth(3); // 设置线条宽度
+          ctx.setLineWidth(3);
 
           // 绘制矩形框
           ctx.beginPath();
